dbd42cf3c42cbc3afaafb3816bbe1c93e0469088
brendaoby <brendaekemezie@gmail.com>
2022-08-07 14:52:46 -0700

    _printf

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format
+ * @format: Character string
+ * Return: Length of formatted output
+ */
+
+int _printf(const char *format, ...)
+{
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arg;
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
+
+	va_start(arg, format);
+	if (!format || (format[0] == '%' && !format[1]))
+	{
+		return (-1);
+	}
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
+		return (-1);
+	}
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+					? pfunc(arg, &flags)
+					: _printf("%%%c", *p);
+		}
+		else
+		{
+			count += _putchar(*p);
+		}
+	}
+	_putchar(-1);
+	va_end(arg);
+	return (count);
+}
